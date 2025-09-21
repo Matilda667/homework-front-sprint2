65bb0334f32831c9f847be7368d906dc3844cdf6
Maria <165724856+Matilda667@users.noreply.github.com>
2025-09-21 15:56:54 +0200

    hw03

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,34 +4,34 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[] // need to fix any
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
     filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
     const setAll = () => {
-        // need to fix
+        props.setFilter('all') // need to fix
     }
     const setHigh = () => {
-        // need to fix
+        props.setFilter('high') // need to fix
     }
     const setMiddle = () => {
-        // need to fix
+        props.setFilter('middle') // need to fix
     }
     const setLow = () => {
-        // need to fix
+        props.setFilter('low') // need to fix
     }
 
-    const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
-    const cnHigh = s.button + ' ' + s.high + (props.filter === 'high' ? ' ' + s.active : '')
-    const cnMiddle = s.button + ' ' + s.middle + (props.filter === 'middle' ? ' ' + s.active : '')
-    const cnLow = s.button + ' ' + s.low + (props.filter === 'low' ? ' ' + s.active : '')
+    const cnAll = `${s.button} ${s.all} ${props.filter === 'all' ? ' ' + s.active : ''}`
+    const cnHigh = `${s.button} ${s.high} ${props.filter === 'high' ? ' ' + s.active : ''}`
+    const cnMiddle = `${s.button} ${s.middle} ${props.filter === 'middle' ? ' ' + s.active : ''}`
+    const cnLow = `${s.button} ${s.low} ${props.filter === 'low' ? ' ' + s.active : ''}`
 
     const mappedAffairs = props.data.map((a: AffairType) => (
         <Affair
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+            key={a._id}
             affair={a}
             deleteAffairCallback={props.deleteAffairCallback}
         />
@@ -40,34 +40,11 @@ function Affairs(props: AffairsPropsType) {
     return (
         <div>
             <div className={s.buttonContainer}>
-                <button
-                    id={'hw2-button-all'}
-                    onClick={setAll}
-                    className={cnAll}
-                >
-                    All
-                </button>
-                <button
-                    id={'hw2-button-high'}
-                    onClick={setHigh}
-                    className={cnHigh}
-                >
-                    High
-                </button>
-                <button
-                    id={'hw2-button-middle'}
-                    onClick={setMiddle}
-                    className={cnMiddle}
-                >
-                    Middle
-                </button>
-                <button
-                    id={'hw2-button-low'}
-                    onClick={setLow}
-                    className={cnLow}
-                >
-                    Low
-                </button>
+                <button id={'hw2-button-all'} onClick={setAll} className={cnAll}>All</button>
+                <button id={'hw2-button-high'} onClick={setHigh} className={cnHigh}>High</button>
+                <button id={'hw2-button-middle'} onClick={setMiddle} className={cnMiddle}>Middle</button>
+                <button id={'hw2-button-low'} onClick={setLow} className={cnLow}>Low</button>
+
             </div>
             <div className={s.affairs}>{mappedAffairs}</div>
         </div>
--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -1,53 +1,77 @@
-import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
-import Greeting from './Greeting'
-import { UserType } from './HW3'
+import React, { useState, ChangeEvent, KeyboardEvent } from 'react';
+import Greeting from './Greeting';
+import { UserType } from './HW3';
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: UserType[];
+    addUserCallback: (name: string) => void;
 }
 
-export const pureAddUser = (name: any, setError: any, setName: any, addUserCallback: any) => {
-    // если имя пустое - показать ошибку, иначе - добавить юзера и очистить инпут
+export const pureAddUser = (
+    name: string,
+    setError: (error: string) => void,
+    setName: (name: string) => void,
+    addUserCallback: (name: string) => void
+) => {
+    if (name.trim() === '') {
+        setError('Ошибка! Введите имя!');
+    } else {
+        addUserCallback(name.trim());
+        setName('');
+    }
+}
+    // если имя пустое - показать ошибку: setError('Ошибка! Введите имя!'),
+    // иначе - добавить юзера при помощи addUserCallback и очистить инпут засетав ''
+    // проверить на пустоту можно при помощи метода trim(). К примеру: name.trim() !== ''
+    // ЕСЛИ НЕ БУДЕТ ПОЛУЧАТЬСЯ, НЕ РАССТРАИВАЙСЯ. НА ЧЕТВЕРТОМ ЗАНЯТИИ ПО ТУДУЛИСТУ НАУЧИМ),
+    // НО ВСЕ ТАКИ ПОПЫТАЙСЯ))
+
+
+export const pureOnBlur = (name: string, setError: (error: string) => void) => {
+    if (name.trim() === '') setError('Ошибка! Введите имя!');
 }
 
-export const pureOnBlur = (name: any, setError: any) => { // если имя пустое - показать ошибку
+export const pureOnEnter = (
+    e: KeyboardEvent<HTMLInputElement>,
+    addUser: () => void
+) => {
+    if (e.key === 'Enter') addUser();
 }
+    // если нажата кнопка Enter - добавить
 
-export const pureOnEnter = (e: any, addUser: any) => { // если нажата кнопка Enter - добавить
-}
 
 // более простой и понятный для новичков
 // function GreetingContainer(props: GreetingPropsType) {
 
 // более современный и удобный для про :)
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
-    users,
-    addUserCallback,
-}) => {
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({ users, addUserCallback }) => {
+
     // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+        const [name, setName] = useState<string>(''); // need to fix any
+        const [error, setError] = useState<string>(''); // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('some name') // need to fix
-
-        error && setError('')
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value);
+        if (error) setError('');
     }
     const addUser = () => {
+        // это всего лишь функция стрелочник- она всего лишь получает
+        //сигнал из компоненты <Greeting/> и вызывает pureAddUser (с кучей аргументов)
+        // ЗДЕСЬ НИЧЕГО ПИСАТЬ НЕ НУЖНО-ВСЕ ОК
+
         pureAddUser(name, setError, setName, addUserCallback)
     }
 
     const onBlur = () => {
+        // все тоже самое, что и в addUser -функция стрелочник
+        // всего лишь получает сигнали из компоненты <Greeting/> и вызывает pureOnBlur (с кучкой аргументов)
         pureOnBlur(name, setError)
     }
 
-    const onEnter = (e: any) => {
-        pureOnEnter(e, addUser)
-    }
+    const onEnter = (e: KeyboardEvent<HTMLInputElement>) => pureOnEnter(e, addUser);
 
-    const totalUsers = 0 // need to fix
-    const lastUserName = 'some name' // need to fix
+    const totalUsers = users.length // need to fix
+    const lastUserName = users.length > 0 ? users[users.length - 1].name : '' // need to fix
 
     return (
         <Greeting
